4f9c18cfc00bc33dd40f9e39e50e0100b45b5cb2
Yashar Ghaffari <123151606+Yashar7800@users.noreply.github.com>
2025-05-21 21:25:37 +0330

    Create date.py

---
+++ date.py
@@ -0,0 +1,9 @@
+import datetime 
+
+def print_date():
+  current_date = datetime.datetime.now()
+  return print(f'Hello today is {current_date} goodluck!')
+
+if __nmae__ == "print_date":
+  main()
+  
